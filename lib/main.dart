d5af2c4d1cf6054bbf739c7d7e9c6d12608aa42c
unknown <shamjadkarattiyattil@gmail.com>
2024-05-06 15:17:08 +0530

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:demo_project/view/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    // ignore: prefer_const_constructors
+    return MaterialApp(
+      title: 'Flutter Demo',
+     
+      home: const HomePage(),
+    );
+  }
+}
+
